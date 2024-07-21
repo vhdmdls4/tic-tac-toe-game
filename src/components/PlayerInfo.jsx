1c536606532eb8fdc6c2a448e85cd46c1bd6f860
Victor <vhduartelima@hotmail.com>
2024-07-20 23:52:03 -0300

    feat: added playerinfo

---
+++ src/components/PlayerInfo.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+// interface PlayerProps {
+//   name: string;
+//   symbol: string;
+// }
+
+export default function PlayerInfo({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name ?? "");
+
+  function handleClick() {
+    //updating state based on last state should be called with a arrow fcuntion
+    setIsEditing((prevValue) => !prevValue);
+  }
+
+  function handleInputChange(event) {
+    setPlayerName(event?.target.value);
+  }
+
+  let editableName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editableName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleInputChange}
+      />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editableName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}

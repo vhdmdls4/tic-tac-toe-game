87371728c1b330081120620636c42c1e832ffb24
Victor <vhduartelima@hotmail.com>
2024-07-22 09:42:15 -0300

    update: added gameBoard type just to document

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,31 @@
 /**
- * @typedef {"X" | "O" | null} FieldType
+ * @typedef {"X" | "O" | null} PlayerType
  */
 
-/** @enum {FieldType} */
-export const fieldTypes = {
+/** @enum {PlayerType} */
+export const PlayerTypes = {
   X: "X",
   O: "O",
   EMPTY: null,
 };
+
+/**
+ * @typedef {"X" | "O" | null} PlayerTypes
+ */
+
+/**
+ * Represents a single square on the game board.
+ * @typedef {{row: number, col: number, player: typeof PlayerTypes}} GameBoardSquare
+ */
+
+/**
+ * Represents the entire game board as an array of GameBoardSquare objects.
+ * @typedef {GameBoardSquare[]} GameBoard
+ */
+
+/** @type {GameBoard} */
+// export const GameBoardType = [{
+//   row: Number,
+//   col: Number,
+//   player: typeof PlayerTypes
+// }];
